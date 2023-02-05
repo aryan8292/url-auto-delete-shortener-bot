0c803c308d8047b05bdfb6122df594cef46c83a2
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2023-02-06 02:20:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -82,10 +82,10 @@ if SELF_DELETE == "True":
 
     # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://youtu.be/A6_YkUKgbgo"
+DOWNLOAD_TEXT_URL = "https://t.me/aryan_101082"
 
    # Custom Caption Under Button #
-CAPTION_BUTTON = "Subscribe"
-CAPTION_BUTTON_URL = "https://youtube.com/@GreyMattersYT"
+CAPTION_BUTTON = "💣Subscribe💣"
+CAPTION_BUTTON_URL = "https://t.me/ary_botz"
 
    # Auto Delete For Bot Sending Files #
